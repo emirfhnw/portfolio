26e665eaf2576aed36d53481bbb07a90fa02de02
Emircan Bagcivan <emircan.bagcivan@students.fhnw.ch>
2025-12-29 19:14:31 +0100

    Serve media files with WhiteNoise

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from whitenoise import WhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
+
+# Serve MEDIA files in production too (Render/Gunicorn)
+application = WhiteNoise(application, root=settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
